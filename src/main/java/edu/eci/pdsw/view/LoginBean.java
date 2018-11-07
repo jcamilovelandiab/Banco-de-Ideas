7a2c547077262fe1ee5639e5561703537621ea7b
CrkJohn <JhonhRge@gmail.com>
2018-11-06 23:59:31 -0500

    LOGIN dudoso

--- src/main/java/edu/eci/pdsw/view/LoginBean.java
+++ src/main/java/edu/eci/pdsw/view/LoginBean.java
@@ -1,4 +1,5 @@
 package edu.eci.pdsw.view;
+import java.io.IOException;
 import java.sql.Date;
 import java.util.Calendar;
 import java.util.List;
@@ -9,6 +10,8 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+
+import edu.eci.pdsw.samples.services.ServicesException;
 import edu.eci.pdsw.samples.services.impl.*;
 
 import edu.eci.pdsw.entities.*;
@@ -22,6 +25,18 @@ public class LoginBean {
 	
 	@Inject
 	private IdeasServicesImpl services;
+	
+	public void login(String correo) throws IOException, ServicesException{
+			System.out.println(correo);
+			Usuario user = services.consultarUsuario(correo);
+            if (user.getTipo().equals(Rol.ADMINISTRADOR)) {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("adminitracion.xhtml");
+            }
+            
+        
+		
+	}
+
 	
 
 
