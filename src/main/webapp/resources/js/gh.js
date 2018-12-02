30bfd6b4411aff40ab8c697e23a00dc642743b36
CrkJohn <JhonhRge@gmail.com>
2018-12-02 15:08:49 -0500

    Bottom like funciona, despues de un 1 queda en 1, no se reinicia

--- src/main/webapp/resources/js/gh.js
+++ src/main/webapp/resources/js/gh.js
@@ -1,10 +1,23 @@
 
 $(document).on("click", ".votos", function () {
      var lik = $(this).data('id');
-   
+     
      $(".modal-body #lik").val( lik );
+
 });
 
 $("#ModalVoto").on('shown.bs.modal', function(){
-	$("#votos_form\\:lik").val($("#lik").val())
+	$("#votos_form\\:lik").val($("#lik").val());
+
+});
+
+$('td').click(function(){
+    var colIndex = $(this).parent().children().index($(this));
+    var rowIndex = $(this).parent().parent().children().index($(this).parent());
+    if(colIndex == 6){
+    	var voto = $("#TablaIni tr:eq("+rowIndex+") td:last .g").data('id');
+    	if(voto==true){
+    		$("#ts4").prop('checked','true');
+    	}
+    }
 });
