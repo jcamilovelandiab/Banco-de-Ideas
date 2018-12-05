56bacd9e8167ca4259fdeec4c8ee8fefa07583c1
Alejoguzm07 <alejoguzm07@gmail.com>
2018-12-05 18:28:33 -0500

    Sirve pero no sirve

--- src/main/webapp/resources/js/gh.js
+++ src/main/webapp/resources/js/gh.js
@@ -1,4 +1,4 @@
-
+//Votos
 $(document).on("click", ".votos", function () {
      var lik = $(this).data('id');
      
@@ -25,7 +25,6 @@ $('td').click(function(){
     }
 });
 
-
 $(".ntb").click(function(){
 	var estilo = $(".ntb").attr('style');
 	var estiloSplit = estilo.split(" ");
@@ -38,6 +37,46 @@ $(".ntb").click(function(){
 	$(".oculto").click();
 });
 
-$('#cancelar').click(function(){
-	localition
+//Mias
+
+$(document).on("click", ".mias", function () {
+     var lik = $(this).data('id');
+     
+     $(".modal-body #mia").val( lik );
+
+});
+
+$("#ModalMias").on('shown.bs.modal', function(){
+	$("#mias_form\\:mia").val($("#mia").val());
+
+});
+
+
+
+$('td').click(function(){
+
+    var colIndex = $(this).parent().children().index($(this));
+    var rowIndex = $(this).parent().parent().children().index($(this).parent());
+    if(colIndex == 6){
+    	var voto = $("#TablaIniP tr:eq("+(rowIndex+1)+") td:last .p").data('id');
+    	if(voto==true){
+    		$(".mia").attr('style','background-color: #900; color:white;');
+    	}else{
+    		$(".mia").attr('style','background-color: white; color:black;');
+    	}
+    }
+});
+
+
+
+$(".mia").click(function(){
+	var estilo = $(".mia").attr('style');
+	var estiloSplit = estilo.split(" ");
+	//["background-color:", "#900;", "color:white"]
+	if(estiloSplit[1]=='#900;'){
+		$(".mia").attr('style','background-color: white; color:black;');
+	}else{
+		$(".mia").attr('style','background-color: #900; color:white;');
+	}
+	$(".escondido").click();
 });
